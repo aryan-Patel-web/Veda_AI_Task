61e219501013f42e9b0e0ad82e7baed7318a03fc
aryan-Patel-web <aryanpatel77462@gmail.com>
2026-05-27 17:50:30 +0530

    email code    not sending --- fixx

--- backend/src/utils/pdf.ts
+++ backend/src/utils/pdf.ts
@@ -18,7 +18,13 @@ export const generateExamPdfBuffer = async (
 ): Promise<Buffer> => {
     const browser = await puppeteer.launch({
         headless: true,
-        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu",
+            "--single-process",
+        ],
     });
     try {
         const page = await browser.newPage();
